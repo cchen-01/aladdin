8b59bb477fcd40930d84b01f39d2f24ef6108596
Chelcee Chen <cchen@polaronsolar.com>
2025-07-29 11:51:25 -0400

    fix

--- routes/submit-contact.js
+++ routes/submit-contact.js
@@ -3,6 +3,19 @@ const { google } = require('googleapis');
 const router = express.Router();
 const sheets = google.sheets('v4');
 const key = require('./credentials/credentials.json');
+const credentials = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+};
+
 
 const spreadsheetId = '1Hv9QoNyFBc4dutiElFpmIDoJmCK0UXKj8YtDNHRevAA';
 
@@ -17,7 +30,7 @@ router.post('/submit-contact', async (req, res) => {
 
         // Google Auth setup
         const auth = new google.auth.GoogleAuth({
-            credentials: key,
+            credentials,
             scopes: ['https://www.googleapis.com/auth/spreadsheets']
         });
         const authClient = await auth.getClient();
